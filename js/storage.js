1d6e35b8449bb37a6b694b24eee6d205a0a80259
Phantom-maxx <redneved.0@gmail.com>
2026-05-19 03:55:26 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -20,3 +20,26 @@ export async function getAllFiles() {
     const db = await dbPromise;
     return db.getAllKeys('files');
 }
+
+export async function saveInstalledLib(lib) {
+
+    const db = await dbPromise;
+
+    let libs = await db.get('files', '__libs__');
+
+    libs = libs || [];
+
+    if (!libs.includes(lib)) {
+
+        libs.push(lib);
+
+        await db.put('files', libs, '__libs__');
+    }
+}
+
+export async function getInstalledLibs() {
+
+    const db = await dbPromise;
+
+    return await db.get('files', '__libs__') || [];
+}
