3ee13adb92294adc9b721adf65fab303f6c3fb6e
Phantom-maxx <redneved.0@gmail.com>
2026-05-18 21:04:32 +0530

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,19 +3,35 @@ import { editor } from './editor.js';
 
 const sidebar = document.getElementById('sidebar');
 
+let activeFile = null;
+
 export async function renderSidebar() {
+
     const files = await getAllFiles();
+
     sidebar.innerHTML = '';
 
     files.forEach(file => {
+
         const div = document.createElement('div');
 
         div.className = 'file-item';
+
+        if (activeFile === file) {
+            div.style.background = '#2d72ff33';
+        }
+
         div.textContent = file;
 
         div.onclick = async () => {
+
+            activeFile = file;
+
             const content = await loadFile(file);
+
             editor.setValue(content || '');
+
+            renderSidebar();
         };
 
         sidebar.appendChild(div);
